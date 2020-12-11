51b1ba757843476f48c7ac63b8a81adbb6249b72
Julian Pasquale <julianagustin78@gmail.com>
2020-12-11 16:07:48 -0300

    Initial commit

---
+++ lib/htmx/rails.rb
@@ -0,0 +1,8 @@
+require 'htmx/rails/version'
+
+module Htmx
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/htmx/rails/version.rb
@@ -0,0 +1,5 @@
+module Htmx
+  module Rails
+    VERSION = '0.0.0'.freeze
+  end
+end
